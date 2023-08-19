4d5be8f2099335af721d0e991e1c2769d6ddbfce
Borislav <borislav.tsvetkov02@gmail.com>
2023-08-19 16:41:35 +0300

    add: add more videos to the list

--- tw-project/src/components/Videos/Youtube.tsx
+++ tw-project/src/components/Videos/Youtube.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Box, Flex,} from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Flex, Image, AspectRatio} from "@chakra-ui/react";
 import YouTube from 'react-youtube';
 
 function VideoPage() {
+    const [activeVideo, setActiveVideo] = useState<string | null>(null);
     const videos = ["i4iIz7N8B58", "Qadoh8Utu1I", "MQ8-DYQN0bE",
     "aZJvzkAt-o0", "-3ZvzigPrSQ", "Mh1j26xxc78", "ZdByF_5N8Hs",
     "10M2vbXNzo8", "53IkBcM29SU", "SwMm53KPgpo", "Z4q-w2jLl64",
@@ -12,15 +13,27 @@ function VideoPage() {
         height: '250',
         width: '100%',
         playerVars: {
-            autoplay: 0,
+            autoplay: 1,
         },
     };
 
     return (
         <Flex wrap="wrap" justify="space-between" p={5}>
             {videos.map((videoId) => (
-                <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" m={4} width={{ base: "100%", sm: "45%", md: "30%" }}>
-                    <YouTube videoId={videoId} opts={opts} />
+                <Box key={videoId} maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" m={4} width={{ base: "100%", sm: "45%", md: "30%" }}>
+                    {activeVideo === videoId ? (
+                        <YouTube videoId={videoId} opts={opts} />
+                    ) : (
+                        <AspectRatio ratio={16 / 9}>
+                            <Image
+                                src={`https://img.youtube.com/vi/${videoId}/0.jpg`}
+                                alt="Video Thumbnail"
+                                onClick={() => setActiveVideo(videoId)}
+                                cursor="pointer"
+                                fit="cover"
+                            />
+                        </AspectRatio>
+                    )}
                 </Box>
             ))}
         </Flex>
