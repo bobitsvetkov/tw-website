39890dc909ebfc7b0ebac8abe45560288f05f95f
Borislav <borislav.tsvetkov02@gmail.com>
2023-09-26 12:20:11 +0300

    delete: remove repeated description

--- tw-project/src/components/Challenges/ChallengesPage.tsx
+++ tw-project/src/components/Challenges/ChallengesPage.tsx
@@ -43,7 +43,7 @@ const ChallengesPage = () => {
                             style={cardImageStyle}
                         />
                         <Text fontSize="1rem" color="white" mt="0.5rem">
-                            These challenges are focused more on specific units. These challenges are more focused on individual units rather than factions themselves.
+                            These challenges are more focused on individual units rather than factions themselves. Have fun while you go through these fun challenges!
                         </Text>
                     </Link>
                 </Box>
