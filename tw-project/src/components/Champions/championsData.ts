2c64ceb619c524a961dc1a61eae0d3a89ee911ad
Borislav <borislav.tsvetkov02@gmail.com>
2023-12-17 17:11:32 +0200

    refactor: store champions data in a new file
    this is done to ensure code readability

---
+++ tw-project/src/components/Champions/championsData.ts
@@ -0,0 +1,83 @@
+export const seasons = [
+    {
+        season: "Season 1",
+        winner: "Rising Kings (FightMe and jjj)",
+        runnerUp: "Random X (Hermes and Bloody, Sub: Zero Chilling)",
+        mostPoints: "Rising Kings (Fight Me and jjj: 3.3 PPG)",
+        finalsLink: "https://www.youtube.com/watch?v=dfYzAPUO9IE"
+    },
+    {
+        season: "Season 2",
+        winner: "Rising Kings (FightMe and jjj, Sub: Pepjin)",
+        runnerUp: "PedroPlebDestroyers (Everos and Ross Worthy, Sub: Pedro)",
+        mostPoints: "Random X (Hermes and Bloody, Sub: Zero Chilling: 2.2 PPG)",
+        finalsLink: "https://www.youtube.com/watch?v=uSVtZxNMprA" //placeholder link
+    },
+    {
+        season: "Season 3",
+        winner: "Grass Touchers (WhosMrBubbles and xAizenTTV, Sub: Saturate)",
+        runnerUp: "Parthia's Finest (Kirky and Donald, Sub: BigBees)",
+        mostPoints: "SweetTeaLovers (Moist Blueberry and msheat, Sub: captain: 2.0 PPG)",
+        finalsLink: "https://www.youtube.com/watch?v=dyQL2Wgmdys"
+    },
+    {
+        season: "Season 4",
+        winner: "Parthia's Finest (Kirky and jbarbs, Sub: Donald)",
+        runnerUp: "Gorilla Warfare(Hyena and Dare, Sub: Boles)",
+        mostPoints: "Gorilla Warfare (Hyena and Dare, Sub: Boles: 3.8 PPG)",
+        thirdPlace: "Champions of Roma (MO and Lemerex)",
+        topKD: "1. jbarbs (1.46) 2. Hyena (1.36) 3. Kirky (1.22)",
+        mostChevrons: "1. Hyena (10.9) 2. jbarbs (10.7) 3. Kirky (9.9)",
+        finalsLink: "https://www.youtube.com/watch?v=3788R1n4Xng"
+    },
+    {
+        season: "Season 5",
+        winner: "Gorilla Warfare (Hyena and Dare, Sub: jjj)",
+        runnerUp: "Bubble Bees (BigBees and WhosMrBubbles, Sub: Saturate)",
+        mostPoints: "Tie (Donald's Legion and Bubble Bees: 3.4 PPG)",
+        thirdPlace: "Donald's Legion (Donald and The Centurion, Sub: Everos)",
+        topKD: "1. BigBees (1.33) 2. Donald (1.26) 3. MO (1.25)",
+        mostChevrons: "1. MO and Nungle (10.0) 2. FightMe and Donald (9.92) 3. BigBees (9.73)",
+        finalsLink: "https://www.youtube.com/watch?v=uSVtZxNMprA" //placeholder link
+    },
+    {
+        season: "Season 6",
+        winner: "Rising Kings (FightMe and JackJack, Sub: Rooibos)",
+        runnerUp: "Camel Riders (MarketableSkills and Costanza, Sub: Androo02)",
+        mostPoints: "Perkele (Mia and Hagehi, Sub: AgentPanda: 3.8 PPG)",
+        thirdPlace: "Perkele (Mia and Hagehi, Sub: AgentPanda)",
+        topKD: "1. Bubinni (1.28) 2. Mia (1.27) 3. Donald (1.18)",
+        mostChevrons: "1. Hoax(9.88) 2. Nungle(9.45) 3. rifou(9.29)",
+        finalsLink: "https://youtu.be/XRUFEsNovT4?si=rHvaQ9UpF4qjRF14&t=9889"
+    },
+    {
+        season: "Season 7",
+        winner: "Rice Enjoyers (Nice-Rice and titledsum, Sub: Anthony Duran)",
+        runnerUp: "Gorilla Tactics (Hyena and Bubinni, Sub: jjj)",
+        mostPoints: "Comrades in Arms (Chairman Mao and boopathi: 4.2 PPG)",
+        thirdPlace: "Peppa Pig Fan Club (Pedro and Nungle)",
+        topKD: "1. Bubinni (1.36) 2. Hyena (1.21) 3. jbarbs (1.20)",
+        mostChevrons: "1. rifou (10.67) 2. Bubinni (10.55) 3. Rogal Dorn (10.00)",
+        finalsLink: "https://www.youtube.com/watch?v=VaFPdz1XObQ"
+    },
+    {
+        season: "Season 8",
+        winner: "Parthia's Finest (Jbarbs and Kirky, Sub: BigBees)",
+        runnerUp: "Best or Bust (Dovahkiin and Berry, Sub: Seneys Martell)",
+        mostPoints: "Cimmeria's Chosen (Rogal and Desstro, Sub: Medusa: 4.5 PPG)",
+        thirdPlace: "Gorilla Bois (Hyena and Bubinni, Sub: jjj)",
+        topKD: "1. jbarbs (1.50) 2. Hyena (1.28) 3. Desstro (1.24)",
+        mostChevrons: "1. jbarbs(10.42) 2. Hyena(10.07) 3. Desttro(9.42)",
+        finalsLink: "https://www.youtube.com/watch?v=Zr7FCWC9D2U"
+    },
+    {
+        season: "Season 9",
+        winner: "Best or Bust ( Seneys and Berry, Sub: Bobi)",
+        runnerUp: "Random X (Hermes and Pedrosaur, Sub: Foxas Bat)",
+        mostPoints: "Iron Warriors (Agiad Legacy and Ironfist20, Sub: Foxas Bat: 4.5 PPG)",
+        thirdPlace: "I for Imperium (Illyria and Hami, Sub: HonoredChief)",
+        topKD: "1. Bobi (2.23) 2. Berry (1.19) 3. Hyena (1.15)",
+        mostChevrons: "1. Hyena(12.00) 2. Mia(12.00) 3. Bobi(11.00)",
+        finalsLink: "https://www.twitch.tv/videos/1998531739"
+    },
+];
